cc86b25b17d3cc2a961c01b51747472fa872ffd2
Louis <michael.wei@foxmail.com>
2018-04-26 18:43:40 +0800

    update api location

--- src/renderer/config/api.js
+++ src/renderer/config/api.js
@@ -1,5 +1,5 @@
 //const rootUrl = 'http://127.0.0.1:8360/admin/';
-const rootUrl = 'http://flyinthesky.cn:8360/admin/';
+const rootUrl = 'http://windart.herokuapp.com/admin/';
 const version = 'v0.0.5';
 const isShowDelete = false; // 是否显示删除按钮，上线后的版本不显示
 const api = {
